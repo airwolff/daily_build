c2a2bceb9708611263641dc6596ec78650b4b11d
airwolff <andy.wolff@gmail.com>
2020-12-19 14:37:39 -0600

    hide api

--- 2-infinite-scroll/script.js
+++ 2-infinite-scroll/script.js
@@ -1,4 +1,5 @@
 // Unsplash API
 const count = 10;
-const apiKey = '';
+const apiKey = config.unsplashApi;
+const secretKey = config.unsplashSecretKey;
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
