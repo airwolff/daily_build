2d232da44827309e548eb0278416374867c85b13
airwolff <andy.wolff@gmail.com>
2021-01-22 14:28:54 -0600

    small additing to finish up, skip to next song at finish

--- 8-music-player/script.js
+++ 8-music-player/script.js
@@ -138,12 +138,14 @@ function updateProgressBar(event) {
 // Set Progress Bar
 function setProgressBar(event) {
     const width = this.clientWidth;
-    let progress = event.offsetX;
-    console.log('progress! ', progress );
+    let clickX = event.offsetX;
+    const { duration } = music;
+    music.currentTime = (clickX / width) * duration;
 }
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
 progressContainer.addEventListener('click', setProgressBar);
