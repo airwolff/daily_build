96a390bc53964c865024e42b166ffa9e6b40eff4
airwolff <andy.wolff@gmail.com>
2021-03-02 16:46:20 -0600

    adding form submit functionality

--- 12-countdown/script.js
+++ 12-countdown/script.js
@@ -5,3 +5,12 @@ const dateElement = document.getElementById('date-picker');
 // set date input minimum with todays date
 const today = new Date().toISOString().split('T')[0];
 dateElement.setAttribute('min', today);
+
+// Update Values in Form Input
+function updateCountdown(event) {
+    event.preventDefault();
+    console.log('event = ', event);
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
