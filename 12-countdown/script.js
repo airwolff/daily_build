9920a31d3bf2ba6a312208bee7b0b4f90541edc9
airwolff <andy.wolff@gmail.com>
2021-03-03 12:56:55 -0600

    form done

--- 12-countdown/script.js
+++ 12-countdown/script.js
@@ -2,6 +2,9 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateElement = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // set date input minimum with todays date
 const today = new Date().toISOString().split('T')[0];
 dateElement.setAttribute('min', today);
@@ -9,7 +12,9 @@ dateElement.setAttribute('min', today);
 // Update Values in Form Input
 function updateCountdown(event) {
     event.preventDefault();
-    console.log('event = ', event);
+    countdownTitle = event.srcElement[0].value;
+    countdownDate = event.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
 }
 
 // Event Listeners
